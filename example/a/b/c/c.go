43a3ebd0f95df5824aca20c64041339a0ebbbcf1
Hank Shen <swh@admpub.com>
2023-11-11 18:23:14 +0800

    update

--- example/a/b/c/c.go
+++ example/a/b/c/c.go
@@ -2,5 +2,5 @@ package c
 
 type Coscms struct {
 	Url   string `xorm:"not null default '' VARCHAR(255)" valid:"Requied" form_widget:"text"`
-	Email string `xorm:"not null default '' VARCHAR(255)" valid:"Requied" form_widget:"text"`
+	Email string `xorm:"not null default '' VARCHAR(100)" valid:"Max(100)" form_widget:"radio"`
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/webx-top/tagfast
+
+go 1.21.4
